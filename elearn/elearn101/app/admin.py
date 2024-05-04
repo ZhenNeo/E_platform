e11162220ed3548414a657aebfa28996ad71c3b1
Nikhil <58047550+Nikhil0414@users.noreply.github.com>
2024-05-04 10:35:08 +0530

    Delete elearn directory

--- elearn/elearn101/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Course, Student, Enrollment, Mentorship, QuestionPaper
-
-admin.site.register(Course)
-admin.site.register(Student)
-admin.site.register(Enrollment)
-admin.site.register(Mentorship)
-admin.site.register(QuestionPaper)
-
