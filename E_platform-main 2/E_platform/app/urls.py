ca45023aa466ade486405c102e18f75a110b34f9
Nikhil0414 <nikhilkalkhanda@gmail.com>
2024-05-11 13:46:55 +0530

    New Chnages 101

---
+++ E_platform-main 2/E_platform/app/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+from.views import *
+
+
+urlpatterns = [
+    path('',do_login,name='login'),
+    path('student-signup/',student_signup,name='student-signup'),
+    path('verify-otp',verify_otp,name='verify_otp'),
+    path('logout/', logout_user, name='logout'),
+
+    path('course_catalog', course_catalog, name='course_catalog'),
+    path('mentorship_page/', homepage, name='mentorship_page'),
+    path('paymenthandler/', paymenthandler, name='paymenthandler'),
+    path('course/<int:course_id>/', view_course_details, name='view_course_details'),
+    path('enroll-course/<int:course_id>/', enroll_course, name='enroll_course'),
+    path('course_payment/<int:course_id>/', course_payment, name='course_payment'),
+    path('enrolled-courses/', enrolled_course, name='enrolled_course'),
+    path('enrolled-courses/<int:course_id>/start-course', start_course, name='start_course'),
+    path('start-course/<int:course_id>/previous_question_papers/', previous_question_papers,
+         name='previous_question_papers'),
+
+    path('community/',community_platform, name='community_platform'),
+    path('community/add_post/',add_post, name='add_post'),
+    path('community/add_comment/<int:post_id>/',add_comment, name='add_comment'),
+
+    path('like_post/<int:post_id>/',like_post, name='like_post'),
+    path('dislike_post/<int:post_id>/',dislike_post, name='dislike_post'),
+    path('like_comment/<int:comment_id>/', like_comment, name='like_comment'),
+    path('dislike_comment/<int:comment_id>/', dislike_comment, name='dislike_comment'),
+
+]
